80c5f5731d75739b97e65376caa632b8baa8cd9a
Carina Nuage <carinavbritto@nuageit.com.br>
2024-01-20 20:11:12 -0300

    feat: creates others project pages

---
+++ src/app/details/page.tsx
@@ -0,0 +1,7 @@
+export default function PropertyDetails() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Property Details
+    </main>
+  );
+}
---
+++ src/app/services/page.tsx
@@ -0,0 +1,7 @@
+export default function Services() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Services
+    </main>
+  );
+}
